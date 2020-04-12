ea45b01742300865dd6f6deacd72def61d47c036
tinbolw <tinbolw@gmail.com>
2020-04-12 11:28:32 -0700

    added notes and boolean variable

--- variables.py
+++ variables.py
@@ -1,4 +1,10 @@
 character_name = "John"
+# character name is a string, strings are stored inside quotation marks.
 character_age = "35"
+# you can also put associate a number with a variable by just typing in a number.
+is_Male = True
+# this is a boolean value. it can be true or false. (true and false have to be capitalized. ex: True)
 print("My name is " + character_name + ".")
 print("I am " + character_age + " years old.")
+character_name = "George"
+print("My name is now " + character_name + ".")
