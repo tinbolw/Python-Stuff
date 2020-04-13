8a3fbb35c47742875914df329a3fcacb2b889d8e
tinbolw <tinbolw@gmail.com>
2020-04-13 10:14:27 -0700

    made simple game

---
+++ madlibs game.py
@@ -0,0 +1,11 @@
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+thing = input("Enter a thing: ")
+
+print("Roses are" + " " + color + ",")
+print(plural_noun + " " + "are blue,")
+print("I need " + thing)
+
+
+
