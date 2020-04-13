86390aef8d2fd052d43465952d9f6a1d2788c4f2
tinbolw <tinbolw@gmail.com>
2020-04-13 10:00:57 -0700

    added gathering input from user

---
+++ getting input from user.py
@@ -0,0 +1,12 @@
+
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+# input() gathers input
+print("Hello " + user_name + "! " + "Today you are " + user_age + " years old.")
+
+
+
+
+
+
+
