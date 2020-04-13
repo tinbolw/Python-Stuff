24dd64000ceeb5386504fc4a98b8c20e107f810d
tinbolw <tinbolw@gmail.com>
2020-04-13 10:07:43 -0700

    made super basic calculator

---
+++ simple calculator.py
@@ -0,0 +1,9 @@
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+result = float(num1) + float(num2)
+print(result)
+# int() converts to integer
+# float is a decimal number or integer
+
+
