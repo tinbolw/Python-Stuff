b6905006d12a8711530282c760c81e9c65011b61
tinbolw <tinbolw@gmail.com>
2020-04-13 09:56:10 -0700

    added working with numbers

---
+++ working with numbers.py
@@ -0,0 +1,25 @@
+from math import *
+x = 5
+print(sqrt(4096))
+# you can print numbers with "" or without.
+# you can also do basic math within the brackets
+# mod (%) divides first number by second and gives remainder
+# str() converts to string
+# abs() is absolute value
+# pow is raising a number to the power of another
+# pow(3, 2) = 9
+# max returns largest number (5, 6) = 6
+# min does opposite
+# round rounds numbers
+# (from math) floor always rounds down
+# (from math) ceil always rounds up
+# sqrt returns square root
+
+
+
+
+
+
+
+
+
