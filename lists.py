277d1ccd901ad39be77e2c370f56254555b7dbe8
tinbolw <tinbolw@gmail.com>
2020-04-14 08:39:11 -0700

    added list.py

---
+++ lists.py
@@ -0,0 +1,16 @@
+
+friends = ["Kevin", "Karen", "Keemstar"]
+friends[2] = "Winrar"
+print(friends[0:3])
+# you can print items from an array by indexing
+# you can index with negative numbers [-1]
+# : up to
+# ex 1:3 or 1:
+# you can change values in arrays
+
+
+
+
+
+
+
