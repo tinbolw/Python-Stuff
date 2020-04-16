2b048f5fdd7c5aecba01aba82d233c88fcceca69
tinbolw <tinbolw@gmail.com>
2020-04-16 10:31:30 -0700

    added ifstatements.py

---
+++ ifstatements.py
@@ -0,0 +1,24 @@
+
+is_male = True
+is_tall = True
+
+if is_male and is_tall:
+    print("You are a male and tall")
+elif is_male and not(is_tall):
+    print("You are a short man")
+elif is_tall and not(is_male):
+    print("You are not male but you are tall")
+else:
+    print("You are short, or not male, or both")
+
+
+
+
+
+
+
+
+
+
+
+
