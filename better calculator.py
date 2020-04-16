6d9405698724a9ce3d266bb23951a658c4c8eeb1
tinbolw <tinbolw@gmail.com>
2020-04-16 11:29:36 -0700

    finished better calculator.py

--- better calculator.py
+++ better calculator.py
@@ -1,6 +1,18 @@
 
+num1 = float(input("Enter your first number: "))
+operator = input("What do you want to do?")
+num2 = float(input("Enter your second number: "))
 
-
+if operator == " add"  or operator == "add" or operator == "+" or operator == " +":
+    print(num1 + num2)
+elif operator == " subtract" or operator == "subtract" or operator == "-" or operator == " -":
+    print (num1 - num2)
+elif operator == " divide" or operator == "divide" or operator == "/" or operator == " /":
+    print (num1 / num2)
+elif operator == " multiply" or operator == "multiply" or operator == "*" or operator == " *" or operator == "x" or operator == " x":
+    print (num1 * num2)
+else:
+    print("Invalid operator (try add, subtract, multiply, or divide)")
 
 
 
