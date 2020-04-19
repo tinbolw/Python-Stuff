9d983a0c1f62cc73eb58381532c7297b9612ec80
tinbolw <tinbolw@gmail.com>
2020-04-19 10:17:10 -0700

    added workingwithstrings.py

---
+++ working with strings.py
@@ -0,0 +1,14 @@
+phrase = "string"
+print(phrase.replace("ri", "ra")) # \n to insert new line
+# \ is escape character, use it to type literals
+# like \"
+# + "" adds to string
+# use functions with a period after the variable
+# you can use multiple functions with one variable, but you still have to end the function with brackets.
+# len () to check length
+# some functions are at the beginning of the variable instead of the end.
+# you can use square brackets to get letters (starts with [0])
+# .index check position of characters "parameters"
+# .replace to replace stuff .replace("x", "y")
+# or straight variables
+print("done")
